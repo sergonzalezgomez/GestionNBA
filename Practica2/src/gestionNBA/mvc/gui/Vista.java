6bf91d85accf9278893db47d32c65bbf1fdb7c84
sergonzalezgomez <sergonzalezgomez@gmail.com>
2021-01-22 22:30:30 +0100

    Codificadas las clases Modelo y Vista

--- Practica2/src/gestionNBA/mvc/gui/Vista.java
+++ Practica2/src/gestionNBA/mvc/gui/Vista.java
@@ -4,10 +4,7 @@ import com.github.lgooddatepicker.components.DatePicker;
 import gestionNBA.base.Conferencia;
 import gestionNBA.base.Equipo;
 import gestionNBA.base.Jugador;
-
 import javax.swing.*;
-import java.awt.*;
-import java.util.ResourceBundle;
 
 public class Vista {
     private JFrame frame;
@@ -37,26 +34,56 @@ public class Vista {
     private JButton botAbrir;
     private JButton botUsuarios;
     private JTextField txtNombreEquipo;
-    private JSpinner spnAnnoFundacionEquipo;
     private JTextField txtNombreEstadio;
     private JComboBox cmboxJugadoresEquipo;
     private JButton botCrearEquipo;
     private JButton botSeleccionarFotoEquipo;
     private JTextField txtNombreConferencia;
-    private JSpinner spnAnnoFundacionConferencia;
     private JSpinner spnTitulosConferencia;
     private JComboBox cmboxEquiposConferencia;
     private JButton botCrearConferencia;
     private JRadioButton radioBotConferenciaEste;
     private JRadioButton radioBotConferenciaOeste;
+    private JTextField txtAnnoFundaconEquipo;
+    private JTextField txtAnnoFundacionConferencia;
+
+    DefaultListModel<Jugador> jugadoresDlm;
+    DefaultListModel<Equipo> equiposDlm;
+    DefaultListModel<Conferencia> conferenciasDlm;
+    DefaultComboBoxModel<Jugador> jugadoresDcbm;
+    DefaultComboBoxModel<Equipo> equiposDcbm;
 
     public Vista() {
-        frame = new JFrame("Gestion Alumnos 1.0");
+        frame = new JFrame("NBA");
         frame.setContentPane(panelGeneral);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //frame.setIconImage(new ImageIcon(getClass().getResource("/24x24/edit-user-24.png")).getImage());
+        frame.setIconImage(new ImageIcon(getClass().getResource("/pelotaBaloncesto.png")).getImage());
         frame.setVisible(true);
         frame.setSize(800,520);
         frame.setLocationRelativeTo(null);
+
+        iniciarModelos();
+        controlPorTeclado();
     }
+
+    private void iniciarModelos() {
+        jugadoresDlm = new DefaultListModel<>();
+        equiposDlm = new DefaultListModel<>();
+        conferenciasDlm = new DefaultListModel<>();
+        jugadoresDcbm = new DefaultComboBoxModel<>();
+        equiposDcbm = new DefaultComboBoxModel<>();
+
+        listaJugadores.setModel(jugadoresDlm);
+        listaEquipos.setModel(equiposDlm);
+        listaConferencias.setModel(conferenciasDlm);
+        cmboxJugadoresEquipo.setModel(jugadoresDcbm);
+        cmboxEquipos.setModel(equiposDcbm);
+    }
+
+    private void controlPorTeclado() {
+
+
+
+    }
+
 }
--- Practica2/src/gestionNBA/mvc/modelo/Modelo.java
+++ Practica2/src/gestionNBA/mvc/modelo/Modelo.java
@@ -5,23 +5,75 @@ import gestionNBA.base.Equipo;
 import gestionNBA.base.Jugador;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Modelo {
-    private List<Conferencia> conferencias;
-    private List<Equipo> equipos;
     private List<Jugador> jugadores;
+    private List<Equipo> equipos;
+    private List<Conferencia> conferencias;
 
     public Modelo() {
-        conferencias = new ArrayList<>();
-        equipos = new LinkedList<>();
         jugadores = new LinkedList<>();
+        equipos = new LinkedList<>();
+        conferencias = new ArrayList<>();
     }
 
-    public List<Conferencia> getConferencias() { return conferencias; }
+    public List<Jugador> getJugadores() { return jugadores; }
 
     public List<Equipo> getEquipos() { return equipos; }
 
-    public List<Jugador> getJugadores() { return jugadores; }
+    public List<Conferencia> getConferencias() { return conferencias; }
+
+    public void annadirJugador(Jugador jugador){
+        jugadores.add(jugador);
+        Collections.sort(jugadores);
+    }
+
+    public void annadirEquipo(Equipo equipo){
+        equipos.add(equipo);
+        Collections.sort(equipos);
+    }
+
+    public void annadirConferencia(Conferencia conferencia){
+        conferencias.add(conferencia);
+        Collections.sort(conferencias);
+    }
+
+    public void eliminarJugador(Jugador jugadorEliminado){
+        jugadores.remove(jugadorEliminado);
+    }
+
+    public void eliminarEquipo(Equipo equipoEliminado){ equipos.remove(equipoEliminado); }
+
+    public void eliminarConferencia(Conferencia conferenciaEliminada) { conferencias.remove(conferenciaEliminada); }
+
+    public boolean existeJugador(String nombre, String apellidos){
+        for(Jugador jugador : jugadores){
+            String cadena = jugador.getNombreJugador() + " " + jugador.getApellidosJugador();
+            if(cadena.equalsIgnoreCase(nombre + " " + apellidos)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean existeEquipo(String nombre){
+        for(Equipo equipo : equipos){
+            if(equipo.getNombreEquipo().equalsIgnoreCase(nombre)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean existeConferencia(String nombre){
+        for(Conferencia conferencia : conferencias){
+            if(conferencia.getNombreConferencia().equalsIgnoreCase(nombre)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
